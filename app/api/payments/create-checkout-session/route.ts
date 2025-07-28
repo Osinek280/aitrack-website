36110b40259ecd16527c95602fe4ae05ffa9eccf
Szymon <szymon.osinski.2009@gmail.com>
2025-07-28 20:45:35 +0200

    fix all issues

--- app/api/payments/create-checkout-session/route.ts
+++ app/api/payments/create-checkout-session/route.ts
@@ -6,7 +6,12 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 export async function POST(req: NextRequest) {
   const { userId, email, priceId, subscription } = await req.json();
 
-  try {
+  const customers = await stripe.customers.list({
+    email,
+    limit: 1,
+  });
+
+  if (customers.data.length === 0) {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       line_items: [{ price: priceId, quantity: 1 }],
@@ -16,9 +21,42 @@ export async function POST(req: NextRequest) {
       success_url: `${process.env.FRONTEND_URL}`,
       cancel_url: `${process.env.FRONTEND_URL}`,
     });
+    return NextResponse.json({ type: "checkout", sessionId: session.id });
+  }
+  const customer = customers.data[0];
 
+  // 2. Pobierz subskrypcje klienta
+  const subscriptions = await stripe.subscriptions.list({
+    customer: customer.id,
+    status: "active",
+    limit: 1,
+  });
 
-    return NextResponse.json({ sessionId: session.id });
+  const isSubscribed = subscriptions.data.length > 0;
+
+  try {
+    if (!isSubscribed) {
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: [{ price: priceId, quantity: 1 }],
+        metadata: { userId, email, subscription },
+        customer_email: email,
+        mode: "subscription",
+        success_url: `${process.env.FRONTEND_URL}`,
+        cancel_url: `${process.env.FRONTEND_URL}`,
+      });
+      return NextResponse.json({ type: "checkout", sessionId: session.id });
+    } else {
+      const session = await stripe.billingPortal.sessions.create({
+        customer: customer.id,
+        return_url: `${process.env.FRONTEND_URL}`,
+      });
+
+      return NextResponse.json({
+        type: "billingPortal",
+        sessionId: session.url,
+      });
+    }
   } catch (error) {
     console.error("Error creating checkout session:", error);
     return NextResponse.json({ error: "Failed to create checkout session" });
